ce49de69102c760874eb5ff17da0476487e653b9
Pip543 <140568238+Pip543@users.noreply.github.com>
2023-11-01 09:39:14 +1100

    Update popup.js

--- popup.js
+++ popup.js
@@ -18,6 +18,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const jokes = [
       "Why don't scientists trust atoms? Because they make up everything!",
       "Parallel lines have so much in common. It's a shame they'll never meet.",
+      "Why do seagulls fly over the sea? Because if they flew over the bay they'd be bagels!",
+      "Why didn't the vampire attack Taylor Swift? She had BAD BLOOD (taylors version)",
+      "Why did the chemist read the book on helium so fast? Cause she couldn't put it down",
+      "Why did the two red blood cells break up? Because their love was all in vein",
+      "Why did the biologist break up with the physicist? They had no chemistry",
+      "Wanna hear a joke about paper? Nevermind it's tearable!"
+      "Did you hear the rumour about butter? Well, I'm not going to spread it"
+      "Singing in the shower is fun. Until you get soap in your mouth. Then it becomes a soap opera."
+    
       
     ];
 
